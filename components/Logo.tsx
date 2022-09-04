83fe52d4de19e9b14102056e4bc143d0921d4b72
Mattias Persson <mattias@happypixels.se>
2022-09-04 10:16:15 +0200

    Changed logo and optimized assets

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,8 +5,8 @@ export default function Logo() {
   return (
     <Image
       style={{
-        width: 250,
-        height: 77.16,
+        width: 240,
+        height: 60,
         marginBottom: 50,
       }}
       source={require('../assets/logo.png')}
