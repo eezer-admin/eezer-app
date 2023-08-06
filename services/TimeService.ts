9451063aaff6795db5f9b627ca2cac92caad6e27
Mattias Persson <mattias@happypixels.se>
2023-08-06 16:38:46 +0200

    chore: remove old directories

--- services/TimeService.ts
+++
@@ -1,7 +0,0 @@
-import { parseISO } from 'date-fns';
-
-export function formatDuration(startDate: string, endDate: string): string {
-  const timeDiff = Math.abs(parseISO(endDate) - parseISO(startDate));
-
-  return new Date(timeDiff).toISOString().slice(11, 19);
-}
